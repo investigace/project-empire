c0b6d0f9166439cb13d95fe452f2a1851394b2de
Jan Vlcek <vlki@vlki.cz>
2021-12-19 17:59:54 +0100

    Certbot non-interactive

--- mediawiki/certbot/start.sh
+++ mediawiki/certbot/start.sh
@@ -4,7 +4,7 @@ export DOMAINS_LIST="$DOMAIN"
 
 for loop_domain in ${DOMAINS_LIST}; do
   echo "Obtaining certificate for domain $loop_domain..."
-  certbot certonly --webroot --webroot-path=/var/www/certbot --email $ADMIN_EMAIL --agree-tos --no-eff-email -d $loop_domain
+  certbot certonly --webroot --webroot-path=/var/www/certbot --email $ADMIN_EMAIL --non-interactive --agree-tos --no-eff-email -d $loop_domain
 done
 
 while true; do
