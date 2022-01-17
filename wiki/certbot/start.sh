f67a8e3efb0fab56c6ce4c593a4d556b4a184cd8
Jan Vlcek <vlki@vlki.cz>
2022-01-17 14:14:44 +0100

    Add redirect domain

--- wiki/certbot/start.sh
+++ wiki/certbot/start.sh
@@ -2,6 +2,10 @@
 
 export DOMAINS_LIST="$DOMAIN"
 
+if [ ! -z "$REDIRECT_DOMAIN" ]; then
+  DOMAINS_LIST="$DOMAIN $REDIRECT_DOMAIN"
+fi
+
 for loop_domain in ${DOMAINS_LIST}; do
   echo "Obtaining certificate for domain $loop_domain..."
   certbot certonly --webroot --webroot-path=/var/www/certbot --email $ADMIN_EMAIL --non-interactive --agree-tos --no-eff-email -d $loop_domain
--- wiki/nginx/start.sh
+++ wiki/nginx/start.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-export NGINX_SERVER_NAME=${DOMAIN}
-export NGINX_DEFAULT_HOST=${DOMAIN}
+export DEFAULT_DOMAIN=${DOMAIN}
+export REDIRECT_DOMAIN=${REDIRECT_DOMAIN}
 
-export NGINX_SSL_CERTIFICATE="/etc/letsencrypt/live/${DOMAIN}/fullchain.pem"
-export NGINX_SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${DOMAIN}/privkey.pem"
+export DEFAULT_SSL_CERTIFICATE="/etc/letsencrypt/live/${DEFAULT_DOMAIN}/fullchain.pem"
+export DEFAULT_SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${DEFAULT_DOMAIN}/privkey.pem"
 
-if [ ! -f "$NGINX_SSL_CERTIFICATE" ]; then
-  export NGINX_SSL_CERTIFICATE="/etc/ssl/certs/localhost-selfsigned.crt"
-  export NGINX_SSL_CERTIFICATE_KEY="/etc/ssl/certs/localhost-selfsigned.key"
+if [ ! -f "$DEFAULT_SSL_CERTIFICATE" ]; then
+  export DEFAULT_SSL_CERTIFICATE="/etc/ssl/certs/localhost-selfsigned.crt"
+  export DEFAULT_SSL_CERTIFICATE_KEY="/etc/ssl/certs/localhost-selfsigned.key"
 fi
 
-envsubst '$NGINX_SERVER_NAME,$NGINX_DEFAULT_HOST,$NGINX_SSL_CERTIFICATE,$NGINX_SSL_CERTIFICATE_KEY' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+export REDIRECT_SSL_CERTIFICATE="/etc/letsencrypt/live/${REDIRECT_DOMAIN}/fullchain.pem"
+export REDIRECT_SSL_CERTIFICATE_KEY="/etc/letsencrypt/live/${REDIRECT_DOMAIN}/privkey.pem"
+
+if [ ! -f "$DEFAULT_SSL_CERTIFICATE" ]; then
+  export REDIRECT_SSL_CERTIFICATE="/etc/ssl/certs/localhost-selfsigned.crt"
+  export REDIRECT_SSL_CERTIFICATE_KEY="/etc/ssl/certs/localhost-selfsigned.key"
+fi
+
+envsubst '$DEFAULT_DOMAIN,$DEFAULT_SSL_CERTIFICATE,$DEFAULT_SSL_CERTIFICATE_KEY' < /tmp/default.conf > /etc/nginx/conf.d/default.conf
+
+if [ ! -z "$REDIRECT_DOMAIN" ]; then
+  envsubst '$DEFAULT_DOMAIN,$REDIRECT_DOMAIN,$REDIRECT_SSL_CERTIFICATE,$REDIRECT_SSL_CERTIFICATE_KEY' < /tmp/redirect.conf > /etc/nginx/conf.d/redirect.conf
+else
+  # If we are not using the configuration for redirect domain, make
+  # sure that there is not the old configuration file
+  rm -f /etc/nginx/conf.d/redirect.conf
+fi
+
+nginx -g 'daemon off;'
